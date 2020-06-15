2700c0e4b58c8e3d427f98c7afd1ea9b168f8caf
NguyenHoangKhang090999 <44261606+NguyenHoangKhang090999@users.noreply.github.com>
2020-06-15 21:29:15 +0700

    Update Surface.java

--- src/doan/Surface.java
+++ src/doan/Surface.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package doan;
 
 /**
@@ -68,15 +63,13 @@ public class Surface extends JPanel{
         Axis(g2d);
         if(index==1)
         {
-            int f = 7*15;
-            toi(g2d,a-f,b+7*15,2*15);
-            toi(g2d,a-f,b+7*15,1*15);
-            g2d.setColor(Color.red);
-            g2d.fillOval(a-3*15, b-10*15, 5, 5);
+            int f = 7*5;
             XeTang(g2d);
-            DrawElip(g2d,a+3*15,d+10*15,8*15,2*15);
-            //tinhtien(g2d);
-//            banhxe(g2d,15*15,20*15,2*15);
+            Banhrang(g2d,a+3*5,d+10*5,8*5,2*5);
+            ve4banh(g2d,a-f,b+7*5,2*5);
+            ve4banh(g2d,a-f,b+7*5,1*5);
+            duongbang(g2d);
+
                 
         }
     }
@@ -85,183 +78,22 @@ public class Surface extends JPanel{
     public void Axis(Graphics2D g2d) {
 		g2d.setColor(Color.GRAY);
 		g2d.setStroke(new BasicStroke(1));
-		for (int i =0; i <= 80; i++) {
-			g2d.drawLine(15 * i, 0, 15 * i, 750);// ta se set 15 don vi pixel tren mot square
-			g2d.drawLine(0, 15 * i, 750, 15 * i);
+		for (int i =0; i <= 160; i++) {
+			g2d.drawLine(5 * i, 0, 5 * i, 750);// ta se set 15 don vi pixel tren mot square
+			g2d.drawLine(0, 5 * i, 750, 5 * i);
 		}
 		g2d.setStroke(new BasicStroke(2));
 		g2d.setColor(Color.BLACK);
 		//g2d.drawLine(0, 375, 750, 375);
 		//g2d.drawLine(375, 0, 375,750 );
         }
-    //ham ve dda1
+ 
     
-    public void muiTen(Graphics2D g2d)
-    {
-        //Truong hop 0<m<1 && x1<x2 && y1<y2
-          //ve doan thang 
-            int dx,dy;
-            int xet=1;
-            float Xinc,Yinc,x,y,steps;
-            int tamx,tamy;
-            //MidpointLine(g2d,c,d,c-45,d+30);
-            if(a<c){
-            
-            dy=d-b;
-            dx=c-a;
-            
-            if(dy>dx){
-                steps=Math.abs(dy);
-            }
-            else{
-                steps=Math.abs(dx);
-            }
-            x=a;
-            y=b;
-            Yinc=dy/steps;
-            Xinc=dx/steps;
-            
-            while(steps!=0){
-                if(xet %2 != 0){
-                    //net gach
-                    for(int i=0;i<15;i++){
-                    steps--;
-                    x=x+Xinc;
-                    y=y+Yinc;
-                    g2d.setColor(Color.BLUE);
-                    g2d.fillOval(Math.round(x), Math.round(y), 5, 5);
-                    }   
-                }
-                xet++;
-            }
-            if(b == d){// mui ten nam ngang huong ve ra xa truc oy  -------->
-            muiten1(g2d,c,d,c,d);
-            muiten2(g2d,c,d,c,d);
-            muiten3(g2d,c,d,c,d);
-            }
-         }
-            else if(a>c){
-                dy=b-d;
-                dx=a-c;
-            
-            if(dy>dx){
-                steps=Math.abs(dy);
-            }
-            else{
-                steps=Math.abs(dx);
-            }
-            x=c;
-            y=d;
-            Yinc=dy/steps;
-            Xinc=dx/steps;
-            
-            while(steps!=0){
-                if(xet %2 != 0){
-                    //net gach
-                    for(int i=0;i<15;i++){
-                    steps--;
-                    x=x+Xinc;
-                    y=y+Yinc;
-                    g2d.setColor(Color.BLUE);
-                    g2d.fillOval(Math.round(x), Math.round(y), 5, 5);
-                    }   
-                }
-                xet++;
-            }
-            if(b == d){// mui ten nam ngang huong ve phhia truc oy <-----------
-            muiten4(g2d,c,d,c,d);
-            muiten5(g2d,c,d,c,d);
-            muiten6(g2d,c,d,c,d);
-            }
-            }
-            else if(b < d){
-            dy=d-b;
-            dx=a-c;
-            
-            if(dy>dx){
-                steps=Math.abs(dy);
-            }
-            else{
-                steps=Math.abs(dx);
-            }
-            x=a;
-            y=b;
-            Yinc=dy/steps;
-            Xinc=dx/steps;
-            
-            while(steps!=0){
-                if(xet %2 != 0){
-                    //net gach
-                    for(int i=0;i<15;i++){
-                    steps--;
-                    x=x+Xinc;
-                    y=y+Yinc;
-                    g2d.setColor(Color.BLUE);
-                    g2d.fillOval(Math.round(x), Math.round(y), 5, 5);
-                    }   
-                }
-                xet++;
-            }
-            if(a == c){
-            muiten7(g2d,c,d,c,d);
-            muiten8(g2d,c,d,c,d);
-            muiten9(g2d,c,d,c,d);
-            }
-            }
-            else if(b > d){
-            dy=b-d;
-            dx=c-a;
-            
-            if(dy>dx){
-                steps=Math.abs(dy);
-            }
-            else{
-                steps=Math.abs(dx);
-            }
-            x=c;
-            y=d;
-            Yinc=dy/steps;
-            Xinc=dx/steps;
-            
-            while(steps!=0){
-                if(xet %2 != 0){
-                    //net gach
-                    for(int i=0;i<15;i++){
-                    steps--;
-                    x=x+Xinc;
-                    y=y+Yinc;
-                    g2d.setColor(Color.BLUE);
-                    g2d.fillOval(Math.round(x), Math.round(y), 5, 5);
-                    }   
-                }
-                xet++;
-            }
-            if(a == c){
-            muiten10(g2d,c,d,c,d);
-            muiten11(g2d,c,d,c,d);
-            muiten12(g2d,c,d,c,d); 
-            }
-            }
-            // ve  toa do 
-        int x1 = Math.round(a / 15);
-                    int y1 = Math.round(b / 15);
-                    int x2 = Math.round(c / 15);
-                    int y2 = Math.round(d / 15); 
-                    x1=x1-25;
-                    x2=x2-25;
-                    y1=25-y1;
-                    y2=25-y2;
-                     
-                     g2d.setColor(Color.BLUE);
-                     g2d.setFont(new Font("", Font.BOLD, 20));
-                     g2d.drawString("("+ x1 + "," + y1 + ")", a, b+30);
-                     g2d.drawString("("+ x2 + "," + y2 + ")", c, d+30);
-    }
-    
+
     
     public void XeTang(Graphics2D g2d){
             //ve than xe tang
-            int x = 2*15;
+            int x = 2*5;
             duongthang((Graphics2D) g2d,a,b,c,b);
             duongthang((Graphics2D) g2d,a,b,a,d);
             duongthang((Graphics2D) g2d,c,d,a,d);
@@ -272,54 +104,87 @@ public class Surface extends JPanel{
             duongthang((Graphics2D) g2d,c,d,c+x,b);
             duongthang((Graphics2D) g2d,a,b,c,d);
             duongthang((Graphics2D) g2d,a,d,c,b);
-            duongthang((Graphics2D) g2d,c,d,e,d);
-            duongthang((Graphics2D) g2d,c,d+1*15,e,d+1*15);
-            duongthang((Graphics2D) g2d,c,d+2*15,e,d+2*15);
-            duongthang((Graphics2D) g2d,e,d-1*15,e,d+3*15);
-            duongthang((Graphics2D) g2d,e,d-1*15,e+5*15,d-1*15);
-            duongthang((Graphics2D) g2d,e,d+3*15,e+5*15,d+3*15);
-            duongthang((Graphics2D) g2d,e+5*15,d-1*15,e+5*15,d+3*15);
-            duongthang((Graphics2D) g2d,a-5*15,b,c+6*15,b);
-            duongthang((Graphics2D) g2d,a-5*15,b,a-5*15,b+5*15);
-            duongthang((Graphics2D) g2d,c+6*15,b,c+6*15,b+5*15);
-            duongthang((Graphics2D) g2d,a-10*15,b+5*15,c+10*15,b+5*15);
-            duongthang((Graphics2D) g2d,a-5*15,b,a-10*15,b+5*15);
-            duongthang((Graphics2D) g2d,c+6*15,b,c+10*15,b+5*15);
-            duongthang((Graphics2D) g2d,a-5*15,b+5*15,a-5*15,b+9*15);
-            duongthang((Graphics2D) g2d,a-10*15,b+5*15,a-5*15,b+9*15);
-            duongthang((Graphics2D) g2d,c+6*15,b+9*15,c+10*15,b+5*15);
-            duongthang((Graphics2D) g2d,c+6*15,b+5*15,c+6*15,b+9*15);
-            duongthang((Graphics2D) g2d,a-5*15,b+9*15,c+6*15,b+9*15);
-            //ve dau dan tank
+            
+            duongthang((Graphics2D) g2d,a-5*5,b,c+6*5,b);
+            duongthang((Graphics2D) g2d,a-5*5,b,a-5*5,b+5*5);
+            duongthang((Graphics2D) g2d,c+6*5,b,c+6*5,b+5*5);
+            duongthang((Graphics2D) g2d,a-10*5,b+5*5,c+10*5,b+5*5);
+            duongthang((Graphics2D) g2d,a-5*5,b,a-10*5,b+5*5);
+            duongthang((Graphics2D) g2d,c+6*5,b,c+10*5,b+5*5);
+            duongthang((Graphics2D) g2d,a-5*5,b+5*5,a-5*5,b+9*5);
+            duongthang((Graphics2D) g2d,a-10*5,b+5*5,a-5*5,b+9*5);
+            duongthang((Graphics2D) g2d,c+6*5,b+9*5,c+10*5,b+5*5);
+            duongthang((Graphics2D) g2d,c+6*5,b+5*5,c+6*5,b+9*5);
+            duongthang((Graphics2D) g2d,a-5*5,b+9*5,c+6*5,b+9*5);
             
             
-            duongthang((Graphics2D) g2d,e+5*15,d-1*15,e+7*15,d+1*15);
-            duongthang((Graphics2D) g2d,e+7*15,d+1*15,e+5*15,d+3*15);
+            //ve nong sung cua tank
             
-            //tinh tien dau dan tank
-           // ve banh xe
-        
+            duongthang((Graphics2D) g2d,c,d,c+15*5,d);
+            duongthang((Graphics2D) g2d,c,d+1*5,c+15*5,d+1*5);
+           //ve dau nong
+           g2d.setColor(Color.blue);
+           tomau(g2d,a-5*5,b+5*5,c+6*5,d+3*5);
+           duongthang((Graphics2D) g2d,c+15*5,d,c+16*5,d-1*5);
+           duongthang((Graphics2D) g2d,c+15*5,d+1*5,c+16*5,d+2*5);
+           duongthang((Graphics2D) g2d,c+16*5,d-1*5,c+20*5,d-1*5);
+           duongthang((Graphics2D) g2d,c+16*5,d+2*5,c+20*5,d+2*5);
+           duongthang((Graphics2D) g2d,c+20*5,d-1*5,c+20*5,d+2*5);
+           duongthang((Graphics2D) g2d,c+15*5,d,c+15*5,d+1*5);
+           duongthang((Graphics2D) g2d,c+16*5,d-1*5,c+16*5,d+2*5);
+           duongthang((Graphics2D) g2d,c+17*5,d-1*5,c+17*5,d+2*5);
+           duongthang((Graphics2D) g2d,c+19*5,d-1*5,c+19*5,d+2*5);
+           g2d.setColor(Color.green);
+           tomau(g2d,a,b,c,d);
+           
            
     }
     
-    public void tinhtien(Graphics2D g2d) {
-        int i=0;
-//      while(i<25){
-//                    MyThread mt = new MyThread();
-//                    try {
-//                 mt.Thread1();
-//                 duongthang((Graphics2D) g2d,a+i,d,e+i,d);
-//             } catch (InterruptedException ex) {
-//                 Logger.getLogger(Surface.class.getName()).log(Level.SEVERE, null, ex);
-//             }
-//                    //duongthang((Graphics2D) g2d,e+(5+i)*15,d-1*15,e+(7+i)*15,d+1*15);
-//                   // duongthang((Graphics2D) g2d,e+(7+i)*15,d+1*15,e+(5+i)*15,d+3*15);
-//                    duongthang((Graphics2D) g2d,a+i,d,e+i,d);
-//                    i++;
-//             
-//    }
-duongthang((Graphics2D) g2d,a+i,d,e+i,d);
+    public void duongbang(Graphics2D g2d){
+        
+        duongthang((Graphics2D) g2d,22*5,d+11*5,200*5,d+11*5);
+        duongthang((Graphics2D) g2d,0,d+25*5,200*5,d+25*5);
+        duongthang((Graphics2D) g2d,0*5,d+11*5,3*5,d+11*5);
+       
+        
+        tomau(g2d,6*5,d+17*5,15*5,d+18*5);
+        int a1 = 6;
+        int b1 = 15;
+        int f = 14;
+        for(int i=0;i<10;i++){
+            a1 = a1 + f;
+            b1 = b1 + f;
+            
+            g2d.setColor(Color.white);
+           tomau(g2d,a1*5,d+17*5,b1*5,d+18*5);
+           
+        }
+ }
+    
+    
+    public void hangrao(Graphics2D g2d,int x0,int y0,int x1,int y1){
+        duongthang((Graphics2D) g2d,x0,y0,x1,y1);
+        duongthang((Graphics2D) g2d,x0,y0,x1,y1);
     }
+    
+    public void tomau(Graphics2D g2d,int x0,int y0,int x1,int y1){
+        //to mau 
+        
+           int dy=Math.abs(y1-y0);
+            int dx= Math.abs(x1-x0);
+            int x,y;
+            if(x0>=x1) x=x1;
+            else x=x0;
+            
+            if(y0>=y1) y =y1;
+            else y =y0;
+            
+           g2d.fillRect(x, y, dx, dy);
+    }
+    
+    
+    
+
     
     
     void put8pixel(Graphics2D g2d,int xc, int yc, int x, int y)
@@ -354,12 +219,12 @@ duongthang((Graphics2D) g2d,a+i,d,e+i,d);
     }
 }
     
-     void toi(Graphics2D g2d,int xc,int yc,int r){
+     void ve4banh(Graphics2D g2d,int xc,int yc,int r){
 
          int kc = 0;
          for(int i=0;i<4;i++){
              
-             kc +=4*15;
+             kc +=4*5;
              banhxe(g2d,(xc+kc),yc,r);
             
              
@@ -367,7 +232,7 @@ duongthang((Graphics2D) g2d,a+i,d,e+i,d);
     
 }
      
-      public void DrawElip(Graphics2D g2d,int x_center,int y_center,int a,int b){
+      public void Banhrang(Graphics2D g2d,int x_center,int y_center,int a,int b){
                   float p,a2,b2;
                   int x,y;
                   int check1=1;
@@ -383,7 +248,7 @@ duongthang((Graphics2D) g2d,a+i,d,e+i,d);
                   while(((float)b2/a2)*x<=y)
                   {
                       if(check1 %2 != 0){
-                          for(int i=0;i<15;i++){
+                          for(int i=0;i<5;i++){
                                 putnetdut(g2d,x_center,y_center,x,y);
                             if(p<0)
                             {
@@ -396,7 +261,7 @@ duongthang((Graphics2D) g2d,a+i,d,e+i,d);
                             x++;  
                            }                                               
                       }else{
-                       for(int i=0;i<15;i++){
+                       for(int i=0;i<5;i++){
                              if(p<0)
                              {
                                  p=p+2*((float)b2/a2)*(2*x+3);
@@ -418,7 +283,7 @@ duongthang((Graphics2D) g2d,a+i,d,e+i,d);
                   while(((float)a2/b2)*y<=x)
                   {
                       if(check2 %2 !=0){
-                        for(int i=0;i<15;i++){
+                        for(int i=0;i<5;i++){
                              putnetdut(g2d,x_center,y_center,x,y);
                           if(p<0)
                          {
@@ -431,7 +296,7 @@ duongthang((Graphics2D) g2d,a+i,d,e+i,d);
                           y++;
                         } 
                       }else{
-                        for(int i=0;i<15;i++){
+                        for(int i=0;i<5;i++){
                             if(p<0)
                          {
                            p=p +2*((float)a2/b2)*(2*y+3);
@@ -535,7 +400,7 @@ duongthang((Graphics2D) g2d,a+i,d,e+i,d);
             while(steps!=0){
                 if(xet %2 != 0){
                     //net gach
-                    for(int i=0;i<15;i++){
+                    for(int i=0;i<5;i++){
                     steps--;
                     x=x+Xinc;
                     y=y+Yinc;
